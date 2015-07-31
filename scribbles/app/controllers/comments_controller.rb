4e8f978b3b6449feaaa634d8132d7e1ed875cbcf
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-07-31 10:58:16 -0400

    full CRUD functionality for comments

--- scribbles/app/controllers/comments_controller.rb
+++ scribbles/app/controllers/comments_controller.rb
@@ -5,19 +5,23 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  private
-    def comment_params
-      params.require(:comment).permit(:user_name, :datetime, :comment)
-    end
-
   def edit
     @post = Post.find(params[:post_id])
-    @comment = @post.comments(param[:id])
+    @comment = Comment.find_by(post_id: params[:post_id])
   end
 
   def update
   end
 
   def destroy
+    @comment = Comment.find_by(post_id: params[:post_id])
+    @comment.destroy
+    @post = Post.find(params[:post_id])
+    redirect_to post_path(@post)
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:user_name, :datetime, :comment)
+    end
 end
